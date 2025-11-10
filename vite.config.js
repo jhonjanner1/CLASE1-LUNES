37cf9f5ae6b2b4fa6ce60837c11137ac2f394350
jocmiguelchicoc <jocmiguelcc@gmail.com>
2025-11-10 11:05:59 -0500

    holas x2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/CLASE1-LUNES/", // Cambiado para Netlify
+  build: {
+    outDir: "build", // Cambia la carpeta de salida a "build"
+  },
 });
